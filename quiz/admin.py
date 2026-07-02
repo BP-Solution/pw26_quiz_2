e76aa7c8a52177a4d6b8d69ae256ede01fe59539
Pietroplati <plati.pietro@gmail.com>
2026-07-02 21:58:00 +0200

    prova

--- quiz/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Utente, Quiz, Domanda, Risposta, Partecipazione, RispostaUtenteQuiz
-
-admin.site.register(Utente)
-admin.site.register(Quiz)
-admin.site.register(Domanda)
-admin.site.register(Risposta)
-admin.site.register(Partecipazione)
-admin.site.register(RispostaUtenteQuiz)
